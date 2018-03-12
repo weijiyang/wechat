6e6dab2ae76c9aca2dddbf1ab462df262a58c089
weijiyang <crawling_snail@163.com>
2018-03-12 15:28:37 +0800

    update demo

--- QualityLife/pages/startscence/startscence.js
+++ QualityLife/pages/startscence/startscence.js
@@ -6,28 +6,75 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    "weather":"cloudy",
+    "weather_path":"../../images/weather/cloudy.jpg",
+    "temperature":"12°C",
+    "tools":[
+      {
+        "toolid":'0',
+        "toolName":"NOTE",
+        "toolLogo":"../../images/icon/message.jpg",
+        "toolLogoActive": "../../images/icon/message_selected.jpg",
+        "url":'../notes/notes',
+        "active":true
+      },
+      {
+        "toolid": '1',
+        "toolName": "FOOD",
+        "toolLogo": "../../images/icon/food.jpg",
+        "toolLogoActive": "../../images/icon/food_selected.jpg",
+        "url": '../food/food',
+        "active": true
+      },
+      {
+        "toolid": '2',
+        "toolName": "FOOD",
+        "toolLogo": "../../images/icon/tools.jpg",
+        "toolLogoActive": "../../images/icon/tools_selected.jpg",
+        "url": '../food/food',
+        "active": true
+      },
+      {
+        "toolid": '3',
+        "toolName": "LOVE",
+        "toolLogo": "../../images/icon/loveNode.jpg",
+        "toolLogoActive": "../../images/icon/loveNode.jpg",
+        "url": '../loveNode/loveNode',
+        "active": true
+      },
+      {
+        "toolid": '4',
+        "toolName": "SHOP",
+        "toolLogo": "../../images/icon/shop.jpg",
+        "toolLogoActive": "../../images/icon/shop.jpg",
+        "url": '../shop/shop',
+        "active": true
+      }
+    ]
+  },
+  changeActive:function(e){
+    console.log(e.currentTarget)
   },
   add : function(){
 
 
 
-    var Diary = Bmob.Object.extend("diary");
-    var diary = new Diary();
-    diary.set("title", "hello");
-    diary.set("content", "hello world");
-    //添加数据，第一个入口参数是null
-    diary.save(null, {
-      success: function (result) {
-        // 添加成功，返回成功之后的objectId（注意：返回的属性名字是id，不是objectId），你还可以在Bmob的Web管理后台看到对应的数据
-        console.log("日记创建成功, objectId:" + JSON.stringify(result));
-      },
-      error: function (result, error) {
-        // 添加失败
-        console.log('创建日记失败');
+    // var Diary = Bmob.Object.extend("diary");
+    // var diary = new Diary();
+    // diary.set("title", "hello");
+    // diary.set("content", "hello world");
+    // //添加数据，第一个入口参数是null
+    // diary.save(null, {
+    //   success: function (result) {
+    //     // 添加成功，返回成功之后的objectId（注意：返回的属性名字是id，不是objectId），你还可以在Bmob的Web管理后台看到对应的数据
+    //     console.log("日记创建成功, objectId:" + JSON.stringify(result));
+    //   },
+    //   error: function (result, error) {
+    //     // 添加失败
+    //     console.log('创建日记失败');
 
-      }
-    });
+    //   }
+    // });
 
     // var Diary = Bmob.Object.extend("diary");
     // var query = new Bmob.Query(Diary);
@@ -94,6 +141,10 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+      return{
+          title : '呆呆の日常',
+          desc: '(๑•̀ㅂ•́)و✧解决日常生活遇到的一些小问题，追求优质生活~',
+          path : 'pages/index/index'
+      }
   }
 })
